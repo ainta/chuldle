3908dd2dac9a601ada38a672193c4a8e1183161b
Sunghyeon Jo <ainta@bucketplace.net>
2022-03-02 13:06:41 +0900

    Update Cell.svelte

--- src/Cell.svelte
+++ src/Cell.svelte
@@ -1,6 +1,9 @@
 <script>
 	export let info;
     let classes = "w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white"
+    $: if(info.matched==0){
+        classes += ' absent shadowed bg-slate-400 dark:bg-slate-700 text-white border-slate-400 dark:border-slate-700'
+    }
     $: if(info.matched==1){
         classes += ' correct shadowed bg-green-500 text-white border-green-500';
     }
